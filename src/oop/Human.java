b28c54f3411348f7efdb0973dd1f4be6c6b7219d
Thanh Minh <vmthanh24@gmail.com>
2021-04-13 20:45:04 +0700

    Data structures and algorithms in Java

---
+++ src/oop/Human.java
@@ -0,0 +1,7 @@
+package oop;
+
+public interface Human {
+	public void speak();
+	public void eat();
+	public void work();
+}
---
+++ src/oop/Person.java
@@ -0,0 +1,30 @@
+package oop;
+
+public class Person implements Human {
+	String name;
+	int age;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public void speak() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, my name is "+name);
+		System.out.println("I'm "+age +" years old.");
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		System.out.println("Eating...");
+	}
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		System.out.println("Working...");
+	}
+}
